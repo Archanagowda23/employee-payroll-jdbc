59e35e7aa198b798f9fd1fa7955ee5e7b8c38279
Archanagowda23 <archanar249@gmail.com>
2022-04-18 22:23:33 +0530

    initial commit

---
+++ src/main/java/com/bridgelabz/jdbcemployeepayroll/DBDemo.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.jdbcemployeepayroll;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class DBDemo {
+    public static void main(String[] args) {
+        System.out.println("welcome to employee-payroll mysql");
+
+    }
+}
